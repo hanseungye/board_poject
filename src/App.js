165d01f3f537a78da0da1e51f0d4a8778976edac
hanseungye <comhhj1127@gmail.com>
2025-05-19 00:01:30 +0900

    fix: route root

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Main from './Main/Main'; // 중괄호 없이 원하는 이름(Login)으로 불러옴
-import Login from './Login/Login';
-import Membership from './Membership/Membership';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./Main/Main";
+import Login from "./Login/Login";
+import Membership from "./Membership/Membership";
 
 function App() {
-
-  const basename = process.env.NODE_ENV === "production" ? "/Main":"";
   return (
-    <BrowserRouter basename={basename}>
+    <Router>
       <Routes>
-          <Route path="/" element ={<Main/>}/>
-          <Route path ="/login" element = {<Login/>}/>
-          <Route path='/membership' element = {<Membership/>}/>
+        {/* '/'로 접속해도 Main이 뜨게! */}
+        <Route path="/" element={<Main />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/membership" element={<Membership />} />
+        {/* 기타 라우트 */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
